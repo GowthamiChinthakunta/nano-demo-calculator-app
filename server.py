6a2d1880938a39260c05069ae9064480e2f54ffb
GowthamiChinthakunta <chinthakuntagowthami2002@gmail.com>
2023-08-16 00:06:41 +0530

    examtest

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ def greeting():
 @app.route("/calculator/add", methods=['POST'])
 def add():
     number = request.json
-    result = number['first'] - number['second']
+    result = number['first'] + number['second']
     return {"result": result }
 
 @app.route("/calculator/subtract", methods=['POST'])
@@ -20,5 +20,5 @@ def subtract():
     result = numbers['first'] - numbers['second']
     return {"result": result}
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
